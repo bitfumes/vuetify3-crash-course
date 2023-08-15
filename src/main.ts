2ec2d0494b43604c96eaac101882287d6c44a581
sarthaksavvy <sarthak@bitfumes.com>
2023-08-15 15:55:56 +0530

    [main] installed vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+// Vuetify
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "vuetify/styles";
 
-app.mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+app.use(router);
+app.use(vuetify);
+
+app.mount("#app");
